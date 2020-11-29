275c6687d2ac79ae0b23abc99bf3325038e5889f
Furuka-Sep <furuka0928@gmail.com>
2020-11-29 23:10:54 +0900

    kadai

--- Q06/Main.java
+++ Q06/Main.java
@@ -4,8 +4,12 @@ public class Main{
 		System.out.print("単語を入れてね>");
 		String word=new Scanner(System.in).next();
 		//文字数をもとにchar配列を作成(配列変数名chars)
+		StringBuffer sb =new StringBuffer(word);
+		String wordalt=sb.reverse().toString();
+		char[] chars=new char[word.length];
 		for(int i=0;i<chars.length;i++){
 			//配列にお尻の文字から詰めていく処理
+			chars[i]=wordaltAt(i);
 		}
 		System.out.println(Arrays.toString(chars));
 	}
--- Q11/Main.java
+++ Q11/Main.java
@@ -3,3 +3,4 @@ public class Main{
 	public static void main(String[] args){
 	}
 }
+//問題文が表示されませんでした。
